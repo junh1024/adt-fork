d86184553e1f5a995bf058c731ea4f3844daf1b4
Aaron J. Heller <heller@ai.sri.com>
2014-04-26 14:01:11 -0700

    changes to generate decoders for orders 1, 2 & 3

--- examples/run_allrad_example_IEM_CUBE.m
+++ examples/run_allrad_example_IEM_CUBE.m
@@ -3,23 +3,24 @@ function run_allrad_example_IEM_CUBE
     %
     % see also AMBI_MAT2SPKR_ARRAY, AMBI_RUN_ALLRAD
     
-    %% 
-    % LScoordinate.csv downloaded from 
+    %%
+    % LScoordinate.csv downloaded from
     %   http://ambisonics-symposium.org/symposium2009/audio-infrastructure/loudspeaker-coordinates-csv
-        
-    ambi_run_allrad(...
-        ... % adjust args according to data read from import file
-        ... %   see ambi_mat2spkr_array for coord and unit codes
-        ambi_mat2spkr_array(...
-           ... % speaker location data starts on row 3, column 2 of file
-           ... %  (note DLMREAD is zero based)
-           dlmread('LScoordinates.csv', ',', 2, 1), ...
-           'XYZ',...            % locations are Cartesian X, Y, Z
-           'MMM', ...           % in meters
-           'IEM_CUBE' ...       % name of array
-           ), ...
-        [2,2], ...    % ambisonic order [h, v]
-        [0,0,-1] ... % imaginary speakers at the bottom
-        );
     
+    for order = 1:3
+        ambi_run_allrad(...
+            ... % adjust args according to data read from import file
+            ... %   see ambi_mat2spkr_array for coord and unit codes
+            ambi_mat2spkr_array(...
+            ... % speaker location data starts on row 3, column 2 of file
+            ... %  (note DLMREAD is zero based)
+            dlmread('LScoordinates.csv', ',', 2, 1), ...
+            'XYZ',...            % locations are Cartesian X, Y, Z
+            'MMM', ...           % in meters
+            'IEM_CUBE' ...       % name of array
+            ), ...
+            [order,order], ...    % ambisonic order [h, v]
+            [0,0,-1] ... % imaginary speakers at the bottom
+            );
+    end
     
