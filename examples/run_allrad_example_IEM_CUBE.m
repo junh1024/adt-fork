64bf250560608ac28d1312a3d9587a16842421d2
Aaron J. Heller <heller@ai.sri.com>
2014-05-03 13:40:54 -0700

    moved origin to Z of lowest speaker, approx. ear height

--- examples/run_allrad_example_IEM_CUBE.m
+++ examples/run_allrad_example_IEM_CUBE.m
@@ -7,15 +7,19 @@ function run_allrad_example_IEM_CUBE
     % LScoordinate.csv downloaded from
     %   http://ambisonics-symposium.org/symposium2009/audio-infrastructure/loudspeaker-coordinates-csv
     
+    % speaker location data starts on row 3, column 2 of file
+    %  (note DLMREAD is zero based)
+    % Matthias Kronlachner says +Y points to the right, so fix here
+    S_coords = dlmread('LScoordinates.csv', ',', 2, 1) * diag([1,-1,1]);
+    
+    % The origin of these measurements is on the floor; move it to the
+    % height of lowest speaker, approx ear height for seated listeners
+    S_coords(:,3) = S_coords(:,3) - min(S_coords(:,3));
+    
     for order = 1:3
         ambi_run_allrad(...
-            ... % adjust args according to data read from import file
-            ... %   see ambi_mat2spkr_array for coord and unit codes
             ambi_mat2spkr_array(...
-            ... % speaker location data starts on row 3, column 2 of file
-            ... %  (note DLMREAD is zero based)
-            ... % per Matthias Kronlachner +Y points to the right, so fix here
-            dlmread('LScoordinates.csv', ',', 2, 1) * diag([1,-1,1]), ...
+            S_coords, ...
             'XYZ',...            % locations are Cartesian X, Y, Z
             'MMM', ...           % in meters
             'IEM_CUBE' ...       % name of array
