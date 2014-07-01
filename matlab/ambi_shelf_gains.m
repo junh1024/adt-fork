288a0fbd17a98b7023d640bb22fce1fa78232524
Aaron J. Heller <heller@ai.sri.com>
2014-06-30 21:23:21 -0700

    minor change to ambi_shelf_gains, so you can omit speakers, for schemes that do not depend on number of speakers - rms and amp

--- matlab/ambi_shelf_gains.m
+++ matlab/ambi_shelf_gains.m
@@ -1,34 +1,34 @@
 function [gammas, g0] = ambi_shelf_gains( C, S, type )
-%AMBI_SHELFS  compute shelf gains for Decodage max_rE
-%   C is channel definition struct
-%   S is speaker array definition struct
-%   type is one of:
-%      'energy' for 'conservation de l'énergie totale' [2]
-%      'amp' for 'préservation de l'amplitude' [2]
-%      'rms' for BLaH/Gerzon [1,3]
-%
-% See also AMBI_APPLY_GAMMA
-%
-% References:
-%
-%  [1] M. A. Gerzon, "Practical Perphony: The Reproduction of
-%  Full-Sphere Sound," Preprints of the 65th AES Convention, no. 1571,
-%  p. 11, 1980.
-%
-%  [2] J. Daniel, "Représentation de champs acoustiques, application à
-%  la transmission et à la reproduction de scènes sonores complexes
-%  dans un contexte multimédia," PhD Thesis, 2001.
-%
-%  [3] A. Heller, R. Lee, and E. M. Benjamin, "Is My Decoder
-%  Ambisonic?," AES 125th Convention, San Francisco, pp. 1?21, Dec.
-%  2008.
-%
-%  [4] A. Heller, E. M. Benjamin, and R. Lee, "A Toolkit for the
-%  Design of Ambisonic Decoders," presented at the Linux Audio
-%  Conference 2012, 2012, pp. 1?12.
-
-
-%{
+    %AMBI_SHELFS  compute shelf gains for Decodage max_rE
+    %   C is channel definition struct
+    %   S is speaker array definition struct
+    %   type is one of:
+    %      'energy' for 'conservation de l'énergie totale' [2]
+    %      'amp' for 'préservation de l'amplitude' [2]
+    %      'rms' for BLaH/Gerzon [1,3]
+    %
+    % See also AMBI_APPLY_GAMMA
+    %
+    % References:
+    %
+    %  [1] M. A. Gerzon, "Practical Perphony: The Reproduction of
+    %  Full-Sphere Sound," Preprints of the 65th AES Convention, no. 1571,
+    %  p. 11, 1980.
+    %
+    %  [2] J. Daniel, "Représentation de champs acoustiques, application à
+    %  la transmission et à la reproduction de scènes sonores complexes
+    %  dans un contexte multimédia," PhD Thesis, 2001.
+    %
+    %  [3] A. Heller, R. Lee, and E. M. Benjamin, "Is My Decoder
+    %  Ambisonic?," AES 125th Convention, San Francisco, pp. 1?21, Dec.
+    %  2008.
+    %
+    %  [4] A. Heller, E. M. Benjamin, and R. Lee, "A Toolkit for the
+    %  Design of Ambisonic Decoders," presented at the Linux Audio
+    %  Conference 2012, 2012, pp. 1?12.
+    
+    
+    %{
 This file is part of the Ambisonic Decoder Toolbox (ADT).
 Copyright (C) 2013  Aaron J. Heller
 
@@ -44,45 +44,45 @@ GNU Affero General Public License for more details.
 
 You should have received a copy of the GNU Affero General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
-%}
-
-% Author: Aaron J. Heller <heller@ai.sri.com>
-% $Id$
-
-%%
+    %}
+    
+    % Author: Aaron J. Heller <heller@ai.sri.com>
+    % $Id$
+    
+    %%
     if C.v_order > 0
         windowPoly = @(n)LegendrePoly(n);
     else
         windowPoly = @(n)ChebyshevPoly(n);
     end
-
-    L = length(S.x);
+    
     M = max(C.v_order,C.h_order);
     m = 0:M;
-
+    
     max_rE = max(roots(windowPoly(M+1)));
     Gamma_max_rE = arrayfun(@(n)polyval(windowPoly(n),max_rE), m);
-
+    
     ch_gammas = Gamma_max_rE(C.sh_l+1);
     E_max_rE = ch_gammas * ch_gammas';
-
+    
     switch type
-      case {'energy', 1}
-        g0_max_rE = sqrt( L / E_max_rE );
-      case {'rms', 2}
-        g0_max_rE = sqrt( numel(m) / E_max_rE );
-      case {'amp', 3}
-        g0_max_rE = 1;
-      otherwise
-        display(type);
-        error('type should be 1, 2, 3, ''energy'', ''rms'', or ''amp''');
+        case {'energy', 1}
+            L = length(S.x);
+            g0_max_rE = sqrt( L / E_max_rE );
+        case {'rms', 2}
+            g0_max_rE = sqrt( numel(m) / E_max_rE );
+        case {'amp', 3}
+            g0_max_rE = 1;
+        otherwise
+            display(type);
+            error('type should be 1, 2, 3, ''energy'', ''rms'', or ''amp''');
     end
-
+    
     % et voila!
     gammas = Gamma_max_rE * g0_max_rE;
-
+    
     if nargout > 1
         g0 = g0_max_rE;
     end
-
+    
 end
