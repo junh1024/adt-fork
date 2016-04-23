3482c51d562e06f6b7d532ca7dda998e675c7220
Aaron Heller <heller@ai.sri.com>
2016-04-23 16:45:37 -0700

    python function to write panner gain polynomials.  Imports symYlm and fausicode from symbolic_spherical_harmonics

--- faust/ambi_panner_faust.py
+++ faust/ambi_panner_faust.py
@@ -12,22 +12,26 @@ import symYlm as ssh
 import faustcode as fc
 
 
-def panner_gain_expressions(order, **kwargs):
-    fc.print_faust_vector("pg",
+def panner_gain_expressions(order, varname="panner_gains", **kwargs):
+    fc.print_faust_vector(varname,
                           (fc.faust_code(ssh.Ylm(l,m, **kwargs)) 
                            for l in range(order+1) 
                            for m in range(-l, l+1)))
     
     
-def ambix_panner(order):
-    panner_gain_expressions(order, normalization=ssh.Norm.SN3D, four_pi=True)
+def ambix_panner(order, sph=False):
+    panner_gain_expressions(order, "gains_ACN_SN3D", sph=sph, normalization=ssh.Norm.SN3D, four_pi=True)
     
-def ACN_N3D_panner(order):
-    panner_gain_expressions(order, normalization=ssh.Norm.N3D, four_pi=True)
+def ACN_N3D_panner(order, sph=False):
+    panner_gain_expressions(order, "gains_ACN_N3D", sph=sph, normalization=ssh.Norm.N3D, four_pi=True)
     
     
 def unit_tests():
-    pass
+    max_order = 5;
+    ambix_panner(max_order)
+    ambix_panner(max_order, sph=True)
+    ACN_N3D_panner(max_order)
+    ACN_N3D_panner(max_order, sph=True)
 
 if __name__ == '__main__':
     unit_tests()
