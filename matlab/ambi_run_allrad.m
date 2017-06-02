0815db0bf0b9e0dc36bf448eb52dec23fa8387a5
Aaron Heller <heller@ai.sri.com>
2017-06-02 07:36:08 -0700

    removed extraneous semicolon in ambi_run_allrad.m

--- matlab/ambi_run_allrad.m
+++ matlab/ambi_run_allrad.m
@@ -68,7 +68,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
     
     if ~exist('do_plots','var') || isempty(do_plots)
         do_plots = ~inOctave();
-    end;
+    end
     
     if ~exist('scheme', 'var'), scheme = 'amb'; end;
     
