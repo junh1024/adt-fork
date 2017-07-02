37579026256f7e2299302b0a63f8adf5fff6ef4d
Aaron Heller <heller@ai.sri.com>
2017-07-01 18:07:20 -0700

    reset path and then add ADT and AutoDiff

--- matlab/ambi_optimize_test.m
+++ matlab/ambi_optimize_test.m
@@ -1,12 +1,19 @@
 %% clear ws and load test data
 clear
+restoredefaultpath
+addpath('/Users/heller/Documents/adt/matlab')
+addpath('/Users/heller/Documents/MATLAB/AutoDiff')
+
+%%
+cd('/Users/heller/Documents/adt/examples')
+
 if false
     load('ambi_optimize_test.mat');
 else
-    S = SPKR_ARRAY_CCRMA_LISTENING_ROOM('full');
+    S = SPKR_ARRAY_CCRMA_LISTENING_ROOM('dome');
     %S = speakers_brh_spring2017;
     C = ambi_channel_definitions(3,3,[],'acn', 'n3d');
-    [D, S, M]= ambi_run_allrad(S,C);
+    [D, S, M]= ambi_run_allrad(S,C,[0,0,-1],false);
     
     test_dirs = load(fullfile(ambi_dir('data'), ...
         'Design_5200_100_random.mat'));
